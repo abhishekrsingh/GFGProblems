3f05c51ca5cd9516abff487f8d1299e29f5804d5
abhishekrsingh <91167858+abhishekrsingh@users.noreply.github.com>
2023-12-29 20:21:50 +0530

    Updated Solution - GfG to GitHub

--- Hard/Wildcard string matching/wildcard-string-matching.cpp
+++ Hard/Wildcard string matching/wildcard-string-matching.cpp
@@ -9,29 +9,6 @@ class Solution{
     bool match(string wild, string pattern)
     {
         // code here
-        int i=0;
-        int j=0;
-        while(i<wild.size() and j<pattern.size()){
-            if(wild[i]!=pattern[j] and wild[i]!='*' and wild[i]!='?'){
-                return false;
-            }else if(wild[i]!=pattern[j] and wild[i]=='?'){
-                i++;
-                j++;
-            }else if(wild[i]!=pattern[j] and wild[i]=='*'){
-                while(wild[i]=='*' or wild[i]=='?'){
-                    i++;
-                    j++;
-                }
-                while(pattern[j]!=wild[i]){
-                    j++;
-                }
-            }else{
-                i++;
-                j++;
-            }
-        }
-        if(i!=wild.size() or j!=pattern.size()) return false;
-        return true;
     }
 };
 
