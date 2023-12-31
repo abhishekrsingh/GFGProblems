d4c65c24126c72d6a376fec91cd1ae88fd1060ce
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2023-12-31 21:21:55 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ if-else (Decision Making)/c-ifelse-decision-making.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string compareFive(int n){
+        // code here 
+        return n == 5 ? "Equal to 5" : n < 5 ? "Less than 5" : "Greater than 5";
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.compareFive(N) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
