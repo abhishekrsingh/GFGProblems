09388c38b2b7c9f93ba7b19ae61676b228da59d4
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-01-05 20:52:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reverse a string with spaces intact/reverse-a-string-with-spaces-intact.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+  public:
+  
+        string reverseWithSpacesIntact (string s)
+        {
+            for ( size_t l{}, r{ s.size()-1 }; l < r; )
+                if      ( s[ l ] == ' ' ) ++l;
+                else if ( s[ r ] == ' ' ) --r;
+                else swap( s[ l++ ], s[ r-- ] );
+            return s;
+        }
+      
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout << ob.reverseWithSpacesIntact (s) << endl;
+    }
+    return 0;
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
