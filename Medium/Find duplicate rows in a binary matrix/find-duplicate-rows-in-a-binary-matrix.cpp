70d9bb238850a28c9e69a7bcf6a44c9e2c8c0857
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-01-15 17:07:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find duplicate rows in a binary matrix/find-duplicate-rows-in-a-binary-matrix.cpp
+++ Medium/Find duplicate rows in a binary matrix/find-duplicate-rows-in-a-binary-matrix.cpp
@@ -8,26 +8,45 @@ using namespace std;
 class Solution
 {   
     public:
-    vector<int> repeatedRows(vector<vector<int>> matrix, int M, int N) 
+    // vector<int> repeatedRows(vector<vector<int>> matrix, int M, int N) 
+    // { 
+    //     // Your code here
+    //     vector<int> ans;
+    //     unordered_map<string,int> mm;
+    //     for(int i=0;i<M;i++){
+    //         string temp="";
+    //         for(int j=0;j<N;j++){
+    //             if(matrix[i][j])temp+='1';
+    //             else temp+='0';
+    //         }
+    //         if(mm[temp]){
+    //             ans.push_back(i);
+    //         }
+    //         else{
+    //             mm[temp]=1;
+    //         }
+    //     }
+    //     return ans;
+    // } 
+    
+    
+    vector<int> repeatedRows(vector<vector<int>> &matrix, int M, int N) 
     { 
-        // Your code here
-        vector<int> ans;
-        unordered_map<string,int> mm;
-        for(int i=0;i<M;i++){
-            string temp="";
-            for(int j=0;j<N;j++){
-                if(matrix[i][j])temp+='1';
-                else temp+='0';
-            }
-            if(mm[temp]){
-                ans.push_back(i);
-            }
-            else{
-                mm[temp]=1;
+        vector<int> ans;  // Vector to store the indices of repeated rows
+        set<vector<int>> st;  // Set to store unique rows using vector<int>
+    
+        // Loop through each row of the matrix
+        for(int i = 0; i < M; i++) {
+            // Check if the current row is already in the set (indicating repetition)
+            if(st.find(matrix[i]) != st.end()) {
+                ans.push_back(i);  // If yes, add the index of the current row to the ans vector.
+            } else {
+                st.insert(matrix[i]);  // If no, insert the current row into the set to keep track of unique rows.
             }
         }
-        return ans;
-    } 
+    
+        return ans;  // Return the vector containing indices of repeated rows.
+    }
 };
 
 
