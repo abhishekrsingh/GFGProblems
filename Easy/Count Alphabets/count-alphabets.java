f16fb7c35377df120bb2ee2c1b22d8856c2d9ef5
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-02-02 21:34:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Alphabets/count-alphabets.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            
+            String S = read.readLine();
+            
+
+            Solution ob = new Solution();
+            
+
+            
+            System.out.println(ob.Count(S));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int Count(String S)
+    {
+        int count=0;
+        for(int i=0;i<S.length();i++)
+        {
+            if(Character.isAlphabetic(S.charAt(i)))
+            count++;
+        }
+        return count;
+    }
+}
