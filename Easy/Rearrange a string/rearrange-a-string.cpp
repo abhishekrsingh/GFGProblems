b1d674f335331d2735506d944696a2b654aeb537
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-02-05 10:43:21 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rearrange a string/rearrange-a-string.cpp
+++ Easy/Rearrange a string/rearrange-a-string.cpp
@@ -8,34 +8,57 @@ using namespace std;
 class Solution
 {
   public:
-    string arrangeString(string str)
-    {
-        //code here.
-        // vector<char> mark(26, -1);
-        // int sum = 0;
-        // for(int i = 0; i < length(); i++)
-        // {
-        //     if ('A' <= str[i] && str[i] <= 'Z')
-        //         int 
-        // }
-        string st = "";
+    // string arrangeString(string str)
+    // {
+    //     string st = "";
+    //     int sum = 0;
+    //     sort(str.begin(), str.end());
+    //     for(int i = 0; i < str.length(); i++)
+    //     {
+    //         if ('A' <= str[i] && str[i] <= 'Z')
+    //             st += str[i];
+    //         else
+    //             sum += str[i] - '0';
+    //     }
+    //     string result = to_string(sum);
+    //     if(sum == 0)
+    //     {
+    //         return st;
+    //     }else{
+    //         return st+result;
+    //     }
+    // }
+    
+    string arrangeString(string str) {
+        // Variables to store alphabets and the sum of digits
+        string alphabets = "";
         int sum = 0;
+    
+        // Sort the input string
         sort(str.begin(), str.end());
-        for(int i = 0; i < str.length(); i++)
-        {
-            if ('A' <= str[i] && str[i] <= 'Z')
-                st += str[i];
-            else
+    
+        // Traverse the sorted string
+        for(int i = 0; i < str.length(); i++) {
+            // If the character is an uppercase alphabet, add it to the alphabets string
+            if ('A' <= str[i] && str[i] <= 'Z') {
+                alphabets += str[i];
+            } else {
+                // If the character is a digit, add its numeric value to the sum
                 sum += str[i] - '0';
+            }
         }
+    
+        // Convert the sum to a string
         string result = to_string(sum);
-        if(sum == 0)
-        {
-            return st;
-        }else{
-            return st+result;
+    
+        // If the sum is 0, return only the alphabets, otherwise concatenate the sum to the alphabets
+        if(sum == 0) {
+            return alphabets;
+        } else {
+            return alphabets + result;
         }
     }
+
 };
 
 //{ Driver Code Starts.
