5bdd5b41023aef39accb5a6b1d86c958d3d53ffe
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-02-20 17:15:20 +0530

    Updated Solution - GfG to GitHub

--- Basic/Third largest element/third-largest-element.cpp
+++ Basic/Third largest element/third-largest-element.cpp
@@ -7,9 +7,43 @@ class Solution{
   public:
     int thirdLargest(int arr[], int n)
     {
-        if(n < 3) return -1;
-        sort(arr, arr+n);
-        int third = arr[n-3];
+        // if(n < 3) return -1;
+        // sort(arr, arr+n);
+        // int third = arr[n-3];
+        // return third;
+        
+        /* There should be atleast three elements */
+        if (n < 3) 
+        { 
+            return -1; 
+        } 
+      
+        // Initialize first, second and third Largest element 
+        int first = arr[0], second = INT_MIN, third = INT_MIN; 
+      
+        // Traverse array elements to find the third Largest 
+        for (int i = 1; i < n ; i ++) 
+        { 
+            /* If current element is greater than first, 
+               then update first, second and third */
+            if (arr[i] > first) 
+            { 
+                third  = second; 
+                second = first; 
+                first  = arr[i]; 
+            } 
+      
+            /* If arr[i] is in between first and second */
+            else if (arr[i] > second) 
+            { 
+                third = second; 
+                second = arr[i]; 
+            } 
+      
+            /* If arr[i] is in between second and third */
+            else if (arr[i] > third) 
+                third = arr[i]; 
+        } 
         return third;
     }
 
