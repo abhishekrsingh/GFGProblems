53753f46b32fa99828e79f8df2cec32013792677
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-02-28 15:56:43 +0530

    Updated Solution - GfG to GitHub

--- Basic/Missing number in shuffled array/missing-number-in-shuffled-array.cpp
+++ Basic/Missing number in shuffled array/missing-number-in-shuffled-array.cpp
@@ -8,19 +8,37 @@ using namespace std;
 
 class Solution{
     public:
+    // long long findMissing(long long a[], long long b[], int n)
+    // {
+    //     long long sumA = 0;
+    //     long long sumB = 0;
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         sumA += a[i];
+    //     }
+    //     for(int i = 0; i < n - 1; i++) // Assuming b[] has one element less than a[]
+    //     {
+    //         sumB += b[i];
+    //     }
+    //     return sumA - sumB;
+    // }
+    
     long long findMissing(long long a[], long long b[], int n)
     {
-        long long sumA = 0;
-        long long sumB = 0;
-        for(int i = 0; i < n; i++)
-        {
-            sumA += a[i];
+        long long suma=0;
+        
+        // XOR operation to calculate the sum of elements in array a
+        for(int i=0;i<n;i++){
+          suma^=a[i];
         }
-        for(int i = 0; i < n - 1; i++) // Assuming b[] has one element less than a[]
-        {
-            sumB += b[i];
+    
+        // XOR operation to calculate the sum of elements in array b
+        for(int i=0;i<n-1;i++){
+          suma^=b[i];
         }
-        return sumA - sumB;
+    
+        // Return the missing element
+        return suma;
     }
 };
 
