d2dffdedbeb3c939175dd011d78e94799cbed563
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-10-01 21:25:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -110,28 +110,21 @@ class Node
 class Solution {
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
-    void help(Node root,List<Integer> temp){
-        if(root==null)return;
-        help(root.left,temp);
-        temp.add(root.data);
-        help(root.right,temp);
+    public void find(Node root,List<Integer> ans){
+        if(root == null)return;
+        find(root.left,ans);
+        ans.add(root.data);
+        find(root.right,ans);
     }
-    // Function to return a list of integers denoting the node
-    // values of both the BST in a sorted order.
-    public List<Integer> merge(Node root1, Node root2) {
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
         // Write your code here
-        List<Integer> first = new ArrayList<>();
-        List<Integer> second = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        help(root1,first);
-        help(root2,second);
-        int i=0,j=0;
-        while(i<first.size() && j<second.size()){
-            if(first.get(i)<=second.get(j))ans.add(first.get(i++));
-            else ans.add(second.get(j++));
-        }
-        while(i<first.size())ans.add(first.get(i++));
-        while(j<second.size())ans.add(second.get(j++));
+        find(root1,ans);
+        find(root2,ans);
+        Collections.sort(ans);
         return ans;
     }
 }
