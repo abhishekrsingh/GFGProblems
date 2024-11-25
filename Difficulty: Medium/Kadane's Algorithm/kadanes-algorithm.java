f811d036544c56f872d340be50f94005fc69eb95
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2024-11-25 22:19:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    // arr: input array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int[] arr) {
+        int res = arr[0];
+        int maxEnding = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            // Find the maximum sum ending at index i by either extending 
+            // the maximum sum subarray ending at index i - 1 or by
+            // starting a new subarray from index i
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+          
+            // Update res if maximum subarray sum ending at index i > res
+            res = Math.max(res, maxEnding);
+        }
+        return res;
+    }
+}
