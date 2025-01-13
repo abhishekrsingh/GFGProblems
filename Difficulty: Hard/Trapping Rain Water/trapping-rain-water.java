2189cde8eac48a56d840a740f8b0f41af8216454
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-01-13 22:52:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().maxWater(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int maxWater(int arr[]) {
+        int n= arr.length;
+        int []leftmax = new int[n];
+        int []rightmax = new int[n];
+        leftmax[0] =arr[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(leftmax[i-1],arr[i]);
+        }
+        rightmax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(rightmax[i+1],arr[i]);
+        }
+        int totalwater=0;
+        for(int i=0;i<n;i++){
+            totalwater+=Math.min(leftmax[i],rightmax[i])-arr[i];        
+        }
+        return totalwater;
+    }
+}
