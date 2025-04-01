1bcd8ee954d2a1c40c6e0513950a8d34b56d4f14
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-04-01 21:17:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximize partitions in a String/maximize-partitions-in-a-string.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while (tc-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            int ans = obj.maxPartitions(s);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int maxPartitions(String s) {
+        // code here
+        int[] first=new int[26],last=new int[26];
+        Arrays.fill(first,-1);
+        Arrays.fill(last,-1);
+        for(int i=0;i<s.length();i++){
+            if(first[s.charAt(i)-'a']==-1)
+            first[s.charAt(i)-'a']=i;
+            last[s.charAt(i)-'a']=i;
+        }
+        int count=0;
+        int end=0;
+        for(int i=0;i<s.length();i++){
+            end=Math.max(end,last[s.charAt(i)-'a']);
+            
+            if(i==end){
+                count++;
+            }
+        }
+        return count;
+    }
+}
