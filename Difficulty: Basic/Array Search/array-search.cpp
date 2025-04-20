df4766cb7f061b5cc89d542ac486c1a7fc2f798f
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-04-20 07:05:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.cpp
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to search x in arr
     // arr: input array
     // X: element to be searched for
     int search(vector<int>& arr, int x) {
-
-        // Your code here
-        for(int i =0; i < arr.size(); i++)
+        for(int i = 0; i < arr.size(); i++)
         {
             if(arr[i] == x) return i;
         }
         return -1;
     }
 };
+
 
 //{ Driver Code Starts.
 
