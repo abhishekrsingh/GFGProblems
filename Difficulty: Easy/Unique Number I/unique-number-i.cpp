658440021c1b23cfda810fdfe21ed04b1d200f6d
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-04-22 21:40:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findUnique(vector<int> &arr) {
+        int n = arr.size();
+    
+        int res = 0;
+        
+        // Find XOR of all elements
+        for (int i = 0; i < n; i++) {
+            res = res ^ arr[i];
+        }
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.findUnique(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
