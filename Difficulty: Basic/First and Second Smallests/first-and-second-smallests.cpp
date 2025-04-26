c74ebe8c00e9d81f8536abe1a26562de8b2c9628
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-04-26 23:27:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First and Second Smallests/first-and-second-smallests.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <random>
+#include <sstream>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> minAnd2ndMin(vector<int> &arr) {
+        int smallest = INT_MAX;
+        int secondSmallest = INT_MAX;
+        
+        for (int num : arr) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num > smallest && num < secondSmallest) {
+                secondSmallest = num;
+            }
+        }
+    
+        if (secondSmallest == INT_MAX) {
+            return {-1, -1};
+        }
+        
+        return {smallest, secondSmallest};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.minAnd2ndMin(arr);
+        if (ans[0] == -1)
+            cout << -1 << endl;
+        else
+            cout << ans[0] << " " << ans[1] << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
