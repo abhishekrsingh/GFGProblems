3dc240dae37854ccfd6c6c47500bf9956945dcf7
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-07-26 20:38:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -1,55 +1,26 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().circularSubarraySum(arr));
-            // System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
+    public int maxCircularSum(int arr[]) {
+        // code here
+        int total = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int curMax = 0;
+        int minSum = Integer.MAX_VALUE;
+        int curMin = 0;
 
-    // a: input array
-    // n: size of array
-    // Function to find maximum circular subarray sum.
-    public int circularSubarraySum(int arr[]) {
-        int totalSum = 0;    
-        int currMaxSum = 0, currMinSum = 0;
-        int maxSum = arr[0], minSum = arr[0];
-        
-        for(int i = 0; i < arr.length; i++) {
-          
-            // Kadane's to find maximum sum subarray
-            currMaxSum = Math.max(currMaxSum + arr[i], arr[i]);
-            maxSum = Math.max(maxSum, currMaxSum); 
-          
-            // Kadane's to find minimum sum subarray
-            currMinSum = Math.min(currMinSum + arr[i], arr[i]);
-            minSum = Math.min(minSum, currMinSum);
-            
-            // Sum of all the elements of input array
-            totalSum += arr[i];
+        for (int num : arr) {
+            curMax = Math.max(num, curMax + num);
+            maxSum = Math.max(maxSum, curMax);
+
+            curMin = Math.min(num, curMin + num);
+            minSum = Math.min(minSum, curMin);
+
+            total += num;
         }
-        
-        int normalSum = maxSum;
-        int circularSum = totalSum - minSum;
-        
-        // If the minimum subarray is equal to total Sum
-        // then we just need to return normalSum
-        if(minSum == totalSum)
-            return normalSum;
-        
-        return Math.max(normalSum, circularSum);
+
+        if (maxSum < 0) {
+            return maxSum;
+        }
+
+        return Math.max(maxSum, total - minSum);
     }
 }
