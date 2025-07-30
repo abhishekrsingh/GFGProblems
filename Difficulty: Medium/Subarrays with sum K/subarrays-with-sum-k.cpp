31c850c6e0df45eb1eaa196faa0a6794df62e631
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-07-30 22:18:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -1,55 +1,30 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
   public:
-    int countSubarrays(vector<int> &arr, int k) {
-        unordered_map<int,int>mp;
-        int sum=0,count=0;
-        for(int i=0;i<arr.size();i++){
-            sum+=arr[i];
-            if(sum==k) count++;
-            if(mp.find(sum-k)!=mp.end()) count+=mp[sum-k];
-            mp[sum]++;
+    int cntSubarrays(vector<int> &arr, int k) {
+        // code here
+        unordered_map<int, int> prefixSums;
+  
+        int res = 0;
+        int currSum = 0;
+    
+        for (int i = 0; i < arr.size(); i++) {
+            
+            // Add current element to sum so far.
+            currSum += arr[i];
+    
+            // If currSum is equal to desired sum, then a new
+            // subarray is found. So increase count of subarrays.
+            if (currSum == k)
+                res++;
+    
+            // Check if the difference exists in the prefixSums map.
+            if (prefixSums.find(currSum - k) != prefixSums.end())
+                res += prefixSums[currSum - k];
+    
+            // Add currSum to the set of prefix sums.
+            prefixSums[currSum]++;
         }
-        return count;
+    
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.countSubarrays(arr, k);
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
