0f7c48fdae38e41c199bd7f0a2925ddd79b673ba
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-08-27 20:39:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -1,55 +1,36 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // Function to count the number of possible triangles.
-    int countTriangles(vector<int>& arr) {
-        // code here
-        sort(arr.begin(),arr.end());
-        int n=arr.size(),ans=0;
-        for(int i=n-1;i>=2;i--){
-            int j=0,k=i-1;
-            while(j<k){
-                int twoSideSum = arr[j]+arr[k];
-                if(twoSideSum>arr[i]){
-                    ans+=k-j;
-                    k--;
+    int countTriangles(vector<int> &arr) {
+        int res = 0;
+        sort(arr.begin(), arr.end());
+    
+        // Iterate through the array, fixing 
+        // the largest side at arr[i]
+        for (int i = 2; i < arr.size(); ++i) {
+          
+          	// Initialize pointers for the two smaller sides
+            int left = 0, right = i - 1; 
+    
+            while (left < right) {
+              
+                if (arr[left] + arr[right] > arr[i]) {
+                  
+                    // arr[left] + arr[right] satisfies 
+                    // the triangle inequality, so all pairs
+    				// (x, right) with (left <= x < right) are valid
+                    res += right - left; 
+                  
+                  	// Move the right pointer to check smaller pairs
+                    right--; 
+                } 
+              	else {
+                  
+                  	// Move the left pointer to increase the sum
+                    left++; 
                 }
-                else j++;
             }
         }
-        return ans;
+    
+        return res;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.countTriangles(a) << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
