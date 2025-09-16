97d40c2361ff09f7e71ba01c82208ca1a35c5f13
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-09-16 21:41:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix Evaluation/postfix-evaluation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int floorDiv(int a, int b) {
+        if (a * b < 0 && a % b != 0)
+            return (a / b) - 1;
+        return a / b;
+    }
+    
+    int evaluatePostfix(vector<string>& arr) {
+        stack<int> st;
+    
+        for (string token : arr) {
+            
+            // If it's an operand (number), push it onto the stack
+            if (isdigit(token[0]) || (token.size() > 1 && token[0] == '-')) {
+                st.push(stoi(token));
+            } 
+            
+            // Otherwise, it must be an operator
+            else {
+                int val1 = st.top(); st.pop(); 
+                int val2 = st.top(); st.pop(); 
+    
+                if (token == "+") st.push(val2 + val1);
+                else if (token == "-") st.push(val2 - val1);
+                else if (token == "*") st.push(val2 * val1);
+                else if (token == "/") st.push(floorDiv(val2, val1));
+                else if (token == "^") st.push((int)pow(val2, val1));
+            }
+        }
+        return st.top();
+    }
+};
