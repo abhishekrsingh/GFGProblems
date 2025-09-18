48a7af292a78e408da86701018bed85d32a40e5f
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-09-18 19:10:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,46 +1,13 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
     static String decodeString(String s) {
         // code here
         StringBuilder res = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
-
-            // If the current character is not a closing
-            // bracket, append it to the result string.
             if (s.charAt(i) != ']') {
                 res.append(s.charAt(i));
             }
-
-            // If the current character is a closing bracket
             else {
-
-                // Create a temporary string to store
-                // the substring within the corresponding
-                // opening bracket.
                 StringBuilder temp = new StringBuilder();
                 while (res.length() > 0
                        && res.charAt(res.length() - 1)
