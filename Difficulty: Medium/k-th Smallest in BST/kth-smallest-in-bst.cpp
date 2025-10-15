977b8359b305ce984749947a2413d36faf28eda1
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-10-15 23:10:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
@@ -1,91 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-#define MAX_HEIGHT 100000
-
-// Tree Node
-struct Node {
+/*
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-/*Complete the function below
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
 
     Node(int val) {
         data = val;
@@ -96,47 +14,37 @@ struct Node {
 
 class Solution {
   public:
-    // Return the Kth smallest element in the given BST
-    void inorder(Node* root,int &k,int &ans){
-        if(!root)return ;
-        inorder(root->left,k,ans);
-        k--;
-        if(k==0){
-            ans=root->data;
-            return ;
-        }
-        inorder(root->right,k,ans);
-    }
     int kthSmallest(Node *root, int k) {
-        int ans=-1;
-        inorder(root,k,ans);
-        return ans;
-      
+        // code here
+        int count = 0;
+
+        Node* curr = root;
+        while (curr != nullptr) {
+            if (curr->left == nullptr) {
+                count++;
+                if (count == k) return curr->data;
+                curr = curr->right;
+            } else {
+                // Find the inorder predecessor of curr
+                Node* prev = curr->left;
+                while (prev->right != nullptr && prev->right != curr) {
+                    prev = prev->right;
+                }
+    
+                // Make curr the right child of its inorder predecessor
+                if (prev->right == nullptr) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    count++;
+                    if (count == k) return curr->data;
+    
+                    // Revert the changes made in the tree structure
+                    prev->right = nullptr;
+                    curr = curr->right;
+                }
+            }
+        }
+        return -1;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
-    while (t--) {
-        string s;
-        getline(cin, s);
-        Node* root = buildTree(s);
-
-        getline(cin, s);
-        int k = stoi(s);
-        //  getline(cin, s);
-        Solution obj;
-        cout << obj.kthSmallest(root, k) << endl;
-        // cout<<"~"<<endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
