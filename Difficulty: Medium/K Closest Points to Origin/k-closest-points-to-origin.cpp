c93480e5ec408428a552ec4b9054e001d48a5fd5
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-10-23 20:23:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -1,53 +1,50 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int,vector<int>>>maxHeap;
-        for(auto &i:points){
-            int x=i[0],y=i[1];
-            int dist=x*x + y*y;
-            maxHeap.push({dist,i});
-            if(maxHeap.size()>k) maxHeap.pop();
+    // Function to calculate squared distance from the origin
+    int squaredDis(vector<int>& point) {
+        return point[0] * point[0] + 
+          point[1] * point[1];
+    }
+    
+    // Function to find k closest points to
+    // the origin
+    vector<vector<int>> kClosest(
+            vector<vector<int>>& points, int k) {
+        
+        // Max heap to store points with their 
+        // squared distances
+        priority_queue<pair<int, vector<int>>> maxHeap;
+    
+        // Iterate through each point
+        for (int i = 0; i < points.size(); i++) {
+            int dist = squaredDis(points[i]);
+    
+            if (maxHeap.size() < k) {
+                
+                // If the heap size is less than k, 
+                // insert the point
+                maxHeap.push({dist, points[i]});
+            } else {
+                
+                // If the heap size is k, compare with
+                // the top element
+                if (dist < maxHeap.top().first) {
+                    
+                    // Replace the top element if the
+                    // current point is closer
+                    maxHeap.pop();
+                    maxHeap.push({dist, points[i]});
+                }
+            }
         }
-        vector<vector<int>>ans;
-        while(!maxHeap.empty()){
-            ans.push_back(maxHeap.top().second);
+    
+        // Take the k closest points from the heap
+        vector<vector<int>> res;
+        while (!maxHeap.empty()) {
+            res.push_back(maxHeap.top().second);
             maxHeap.pop();
         }
-        return ans;
+    
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int k;
-        cin >> k;
-        int n;
-        cin >> n;
-        vector<vector<int>> points(n, vector<int>(2));
-        for (int i = 0; i < n; i++) {
-            cin >> points[i][0] >> points[i][1];
-        }
-        Solution ob;
-        vector<vector<int>> ans = ob.kClosest(points, k);
-        sort(ans.begin(), ans.end());
-        for (const vector<int>& point : ans) {
-            cout << point[0] << " " << point[1] << endl;
-        }
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
