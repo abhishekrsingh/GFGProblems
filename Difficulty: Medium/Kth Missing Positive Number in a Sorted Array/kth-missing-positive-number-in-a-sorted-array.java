185b9f7e742c35a3eb438a61dc3938bef683ef74
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-12-27 19:53:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,60 +1,36 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    public int kthMissing(int[] arr, int k) {
-        int i=1;  int j=0;  int count=0;
-        while(true)
-        {
-            if(arr[j]!=i)
-            {
-                count++;
-                if(count==k)
-                {
-                    return i;
-                }
-                i++;
-            }
-            else
-            {
-                if(j==arr.length-1)
-                {
-                    i++;
-                }
-                else
-                {
-                    i++; j++;
-                }
-            }
-            
+    public int kthMissing(int[] nums, int k) {
+        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        int missing = 0;
+        int count = 1;
+       for(int i = 1;i<nums.length;i++){
+           if(nums[i] - nums[i-1] == 1){
+               count++;
+           }
+       }
+        if(count == nums.length && nums[0] == 1){
+                nums[nums.length - 1] = nums[nums.length - 1] + k;
+                return nums[nums.length - 1];
         }
+        else {
+             for (int num : nums) {
+                set.add(num);
+            }
+            int max = Collections.max(set);
+            for (int i = 1; i <=max ; i++) {
+                if (!set.contains(i)) {
+                    list.add(i);
+                }
+            }
+            if(list.size() >= k){
+                missing =  list.get(k - 1);
+            }
+            else{
+                missing = max + (k - list.size());
+            }
+        }
+        return missing;
     }
 }
