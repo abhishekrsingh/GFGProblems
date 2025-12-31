470765fe4906aeaa5076c16976f46f49e1b8b7f2
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2025-12-31 20:21:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
@@ -1,13 +1,4 @@
-//{ Driver Code Starts
-// driver
-
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-/* node for linked list
-
+/*
 class Node {
     int data;
     Node next;
@@ -17,134 +8,99 @@ class Node {
         next = null;
     }
 }
-
 */
 
-import java.math.BigInteger;
-
 class Solution {
-    
-    static Node addTwoLists(Node num1, Node num2) {
-         int carry = 0;
+    public Node reverseLL(Node head){
+        Node past = null;
+        Node present = head;
+        Node future = null;
         
-        Node rev1 = reverse(num1);
-        Node rev2 = reverse(num2);
+        while(present != null){
+            future = present.next;
+            present.next = past;
+            past = present;
+            present = future;
+        }
+        return past;
+    }
+    public Node addTwoLists(Node head1, Node head2) {
+        // code here
+        
+        head1 = reverseLL(head1);
+        head2 = reverseLL(head2);
         
         Node head = null;
-        Node tail = head;
+        Node tail = null;
+        int quo = 0;
+        int rem = 0;
         
-        while(rev1 != null || rev2 != null || carry != 0)
-        {
-            int val1 = 0;
-            int val2 = 0;
-            if(rev1 != null) val1 = rev1.data;
-            if(rev2 != null) val2 = rev2.data;
-            
-            int sum = val1 + val2 + carry;
-           
-            int digit = sum % 10;
-            carry = sum / 10;
-            Node temp = new Node(digit);
-            
+        while((head1 != null) && (head2 != null)){
+            int sum = (head1.data + head2.data + quo);
+            rem = sum % 10;
+            quo = sum / 10;
             if(head == null){
+                Node temp = new Node(rem);
                 head = temp;
                 tail = temp;
             }else{
+                Node temp = new Node(rem);
                 tail.next = temp;
                 tail = temp;
             }
             
-            if(rev1 != null) rev1 = rev1.next;
-            if(rev2 != null) rev2 = rev2.next;
+            head1 = head1.next;
+            head2 = head2.next;
         }
         
-        
-        Node result = reverse(head);
-        
-        while(result != null)
-        {
-            if(result.data != 0)
-            break;
-            result = result.next;
-        }
-        
-        return result;
-    }
-    
-    private static Node reverse(Node head)
-    {
-        Node curr = head;
-        Node prev = null;
-        Node next = null;
-        
-        while(curr != null)
-        {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-}
-
-
-//{ Driver Code Starts.
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int d) {
-        data = d;
-        next = null;
-    }
-}
-
-class GfG {
-
-    static void printList(Node n) {
-        while (n != null) {
-            System.out.print(n.data + " ");
-            n = n.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(read.readLine());
-        while (T-- > 0) {
-
-            String str[] = read.readLine().trim().split(" ");
-            int n = str.length;
-
-            Node num1 = new Node(Integer.parseInt(str[0]));
-            Node tail = num1;
-            for (int i = 1; i < n; i++) {
-                int val = Integer.parseInt(str[i]);
-                tail.next = new Node(val);
-                tail = tail.next;
+        while(head1 != null){
+            int sum = (head1.data + quo);
+            rem = sum % 10;
+            quo = sum / 10;
+            if(head == null){
+                Node temp = new Node(rem);
+                head = temp;
+                tail = temp;
+            }else{
+                Node temp = new Node(rem);
+                tail.next = temp;
+                tail = temp;
             }
-
-            String str2[] = read.readLine().trim().split(" ");
-            int m = str2.length;
-
-            Node num2 = new Node(Integer.parseInt(str2[0]));
-            tail = num2;
-            for (int i = 1; i < m; i++) {
-
-                tail.next = new Node(Integer.parseInt(str2[i]));
-                tail = tail.next;
-            }
-
-            Solution g = new Solution();
-            Node res = g.addTwoLists(num1, num2);
-            printList(res);
-
-            System.out.println("~");
+            head1 = head1.next;
         }
+        while(head2 != null){
+            int sum = (head2.data + quo);
+            rem = sum % 10;
+            quo = sum / 10;
+            if(head == null){
+                Node temp = new Node(rem);
+                head = temp;
+                tail = temp;
+            }else{
+                Node temp = new Node(rem);
+                tail.next = temp;
+                tail = temp;
+            }
+            head2 = head2.next;
+        }
+        if(quo != 0){
+            if(head == null){
+                Node temp = new Node(quo);
+                head = temp;
+                tail = temp;
+            }else{
+                Node temp = new Node(quo);
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+        head = reverseLL(head);
+        Node finalHead = null;
+        
+        while((head != null) && (head.data == 0)){
+            head = head.next;
+        }
+        finalHead = head;
+        return finalHead;
     }
 }
-
-// } Driver Code Ends
