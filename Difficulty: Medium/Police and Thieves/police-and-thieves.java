011eb7a312b37f07cead7b4c389fd3792a49e12a
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-01-15 20:38:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.java
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -1,35 +1,25 @@
 class Solution {
     public int catchThieves(char[] arr, int k) {
-        int n     = arr.length;
-        int i     = 0;    // pointer to scan for 'P'
-        int j     = 0;    // pointer to scan for 'T'
-        int count = 0;    // total thieves caught
-
-        while (i < n && j < n) {
-            // move i to the next policeman
-            while (i < n && arr[i] != 'P') {
-                i++;
+        int n=arr.length;
+        int ans=0, pPos=0, tPos=0;
+        
+        while(pPos<n && tPos<n) {
+            // trying to reach P valid position
+            if(arr[pPos]!='P' || tPos - pPos>k) {
+                pPos++;
+                continue;
             }
-            // move j to the next thief
-            while (j < n && arr[j] != 'T') {
-                j++;
+            
+            // trying to reach T valid position
+            if(arr[tPos]!='T' || pPos - tPos>k) {
+                tPos++;
+                continue;
             }
-
-            // if both pointers are valid and within k distance, catch!
-            if (i < n && j < n && Math.abs(i - j) <= k) {
-                count++;
-                i++;
-                j++;
-            }
-            // if thief is too far left, move thief pointer right
-            else if (j < n && j < i) {
-                j++;
-            }
-            // if policeman is too far left, move policeman pointer right
-            else if (i < n && i < j) {
-                i++;
-            }
+            
+            ans++;
+            pPos++;
+            tPos++;
         }
-        return count;
+        return ans;
     }
 }
