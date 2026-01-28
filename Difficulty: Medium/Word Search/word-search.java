3f2282bc15069ea4de3bff8bf202bc1e0494fe5b
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-01-28 22:30:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.java
+++ Difficulty: Medium/Word Search/word-search.java
@@ -1,68 +1,21 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt(); // Number of test cases
-        while (tc-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char[][] mat = new char[n][m];
-
-            // Reading the matrix
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    mat[i][j] = sc.next().charAt(0);
-                }
-            }
-
-            String word = sc.next();
-            Solution obj = new Solution();
-            boolean ans = obj.isWordExist(mat, word);
-            if (ans)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    static public boolean isWordExist(char[][] mat, String word) {
-         // Code here
+    public boolean isWordExist(char[][] mat, String word) {
+        // Code here
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j] == word.charAt(0)){
-                    boolean[][] visit = new boolean[mat.length][mat[0].length];
-                    if(check(mat, i, j, 0, word, visit))
-                        return true;
+                if(mat[i][j]==word.charAt(0)){
+                    if(rec(i,j,0,mat,word,new boolean[mat.length][mat[0].length])) return true;
                 }
             }
         }
         return false;
     }
-    static boolean check(char[][] mat, int i, int j, int ind, String word, boolean[][] visit){
-        if(ind == word.length()){
-            return true;
-        }
-        if(i<0 || j<0 || i>=mat.length || j>= mat[0].length || visit[i][j] || mat[i][j] != word.charAt(ind)){
-            return false;
-        }
-        visit[i][j]=true;
-        if(check(mat, i, j-1, ind+1, word, visit) ||
-           check(mat, i, j+1, ind+1, word, visit) ||
-           check(mat, i+1, j, ind+1, word, visit) ||
-           check(mat, i-1, j, ind+1, word, visit))
-           return true;
-           
-        visit[i][j]=false;
-        return false;
+    private boolean rec(int i,int j,int k,char mat[][],String word,boolean vis[][]){
+        if(k==word.length()) return true;
+        if(i<0 || i==mat.length || j<0 || j==mat[0].length || vis[i][j] || mat[i][j]!=word.charAt(k)) return false;
+        vis[i][j]=true;
+        boolean possible=rec(i+1,j,k+1,mat,word,vis) || rec(i,j+1,k+1,mat,word,vis) || rec(i-1,j,k+1,mat,word,vis) || rec(i,j-1,k+1,mat,word,vis);
+        vis[i][j]=false;
+        return possible;
     }
 }
