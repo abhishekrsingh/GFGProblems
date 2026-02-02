ac62401ebe8d2dc3730377f2a582d2d5228e996d
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-02-02 22:43:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -2,32 +2,27 @@ class Solution {
   public:
     int maxCircularSum(vector<int> &arr) {
         // code here
-        int totalSum = 0;
-        int currMaxSum = 0, currMinSum = 0;
-        int maxSum = arr[0], minSum = arr[0];
-    
-        for (int i = 0; i < arr.size(); i++) {
-    
-            // Kadane's to find maximum sum subarray
-            currMaxSum = max(currMaxSum + arr[i], arr[i]);
-            maxSum = max(maxSum, currMaxSum);
-    
-            // Kadane's to find minimum sum subarray
-            currMinSum = min(currMinSum + arr[i], arr[i]);
-            minSum = min(minSum, currMinSum);
-    
-            // Sum of all the elements of input array
-            totalSum = totalSum + arr[i];
+        int mx_curr = 0, mxsum = -1e9;
+        int mn_curr = 0, mnsum = 1e9;
+        int n = arr.size(), total = 0;
+        for(int i=0; i<n; i++){
+            total += arr[i];
+            mx_curr += arr[i];
+            mn_curr += arr[i];
+            mxsum = max(mxsum, mx_curr);
+            mnsum = min(mnsum, mn_curr);
+            if(mx_curr < 0){
+                mx_curr = 0;
+            }
+            if(mn_curr > 0){
+                mn_curr = 0;
+            }
         }
-    
-        int normalSum = maxSum;
-        int circularSum = totalSum - minSum;
-    
-        // If the minimum subarray is equal to total Sum
-        // then we just need to return normalSum
-        if (minSum == totalSum)
-            return normalSum;
-    
-        return max(normalSum, circularSum);
+        
+        if(total == mnsum){
+            return mxsum;
+        }
+        
+        return max(mxsum, total-mnsum);
     }
 };
