e14658886600e2868b9104debebb139ad92ed408
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-02-04 20:09:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
@@ -1,45 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int prices[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                prices[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int res = obj.maximumProfit(prices);
-            System.out.println(res);
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public int maximumProfit(int prices[]) {
-        int minSoFar = prices[0];
-        int res = 0;
-    
-        for (int i = 1; i < prices.length; i++) {
-            // Update the minimum value seen so far if we see smaller
-            minSoFar = Math.min(minSoFar, prices[i]);
-            
-            // Update result if we get more profit
-            res = Math.max(res, prices[i] - minSoFar);
+    public int maxProfit(int[] prices) {
+        // Code here
+        int n = prices.length;
+        int maxprofit = 0;
+        int minprice = prices[0];
+        for(int i = 0; i<n; i++){
+            minprice = Math.min(prices[i],minprice);
+            int currprofit = prices[i]- minprice;
+            maxprofit = Math.max(maxprofit,currprofit);
         }
-        return res;
-    }
+        return maxprofit;
+    }  
 }
