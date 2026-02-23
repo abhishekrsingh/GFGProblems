96a3e2314f9d7603dbddbca9a161752fe6ac0b6f
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-02-23 21:59:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -1,43 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
-        while (tc-- > 0) {
-            String s[] = br.readLine().split(" ");
-            int arr[] = new int[s.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = Integer.parseInt(s[i]);
-            }
-            int k = Integer.parseInt(br.readLine());
-
-            Solution obj = new Solution();
-            System.out.println(obj.subarrayXor(arr, k));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public long subarrayXor(int arr[], int k) {
         // code here
-        long ans=0;
-        Map<Integer,Integer> mp = new HashMap();
-        int currXOR=0;
-        mp.put(0,1);
-        for(int i: arr){
-            currXOR=currXOR^i;
-            if(mp.containsKey(currXOR^k))
-                ans+=mp.get(currXOR^k);
-            mp.put(currXOR, mp.getOrDefault(currXOR,0)+1);
+        Map<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+        int prefixXor = 0;
+
+        for (int num : arr) {
+            prefixXor ^= num;
+
+            // Case 1: subarray from start has XOR = k
+            if (prefixXor == k) {
+                count++;
+            }
+
+            // Case 2: subarray ending here has XOR = k
+            int target = prefixXor ^ k;
+            if (freq.containsKey(target)) {
+                count += freq.get(target);
+            }
+
+            // Update frequency of prefixXor
+            freq.put(prefixXor, freq.getOrDefault(prefixXor, 0) + 1);
         }
-        return ans;
+
+        return count;
     }
 }
