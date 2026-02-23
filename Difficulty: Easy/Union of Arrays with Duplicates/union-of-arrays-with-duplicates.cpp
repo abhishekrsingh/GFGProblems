cbbdeeb41a63349aeca33c568d998112d0322aad
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-02-23 22:51:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -1,64 +1,31 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template in C++
-
 class Solution {
   public:
-    // Function to return the count of number of elements in union of two arrays.
-    int findUnion(vector<int>& a, vector<int>& b) {
-        // code here
-        int ans=0;
-        unordered_map<int,int> mm;
-        for(int x:a){
-            if(!mm[x])ans++;
-            mm[x]=1;
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        unordered_set<int> seen;
+        vector<int> result;
+        
+        // Reserve space to avoid reallocations
+        result.reserve(a.size() + b.size());
+        
+        // Process first array
+        for(int x : a) {
+            if(seen.find(x) == seen.end()) {
+                seen.insert(x);
+                result.push_back(x);
+            }
         }
-        for(int x:b){
-            if(!mm[x])ans++;
-            mm[x]=1;
+        
+        // Process second array
+        for(int x : b) {
+            if(seen.find(x) == seen.end()) {
+                seen.insert(x);
+                result.push_back(x);
+            }
         }
-        return ans;
+        
+        // Sort if order is required
+        sort(result.begin(), result.end());
+        
+        return result;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after reading t
-
-    while (t--) {
-        vector<int> a;
-        vector<int> b;
-
-        string input;
-        // For a
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        // For b
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            b.push_back(number);
-        }
-
-        Solution ob;
-        cout << ob.findUnion(a, b) << endl;
-        cout << '~' << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
