fdefdcb653e81c477cd9b026ed76ef0b56649d25
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-02-25 21:15:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -2,32 +2,18 @@ class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
         // Code here
-        int n = arr.size();
+        int n = arr.size(), ans = 0, ct = 0;
         unordered_map<int, int> mp;
-        int ans = 0, sum = 0;
-    
-        for (int i = 0; i < n; ++i)
-        {
-            // Treat elements <= k as -1, elements > k as +1
-            if (arr[i] <= k) sum-- ;
-            else sum++ ;
-    
-            // If overall sum is positive, entire prefix is valid
-            if (sum > 0) ans = i + 1;
-            else
-            {
-                // Check if there was a prefix with sum = current_sum - 1
-                if (mp.find(sum - 1) != mp.end()) {
-                    ans = max(ans, i - mp[sum - 1]);
-                }
-            }
-    
-            // Store the first occurrence of each prefix sum
-            if (mp.find(sum) == mp.end()) {
-                mp[sum] = i;
-            }
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i]>k) ct++;
+            else ct--;
+            
+            if(ct > 0) ans = max(ans, i+1);
+            else if(mp.find(ct-1)!=mp.end()) ans = max(ans, i-mp[ct-1]);
+            
+            if(mp.find(ct)==mp.end()) mp[ct]=i;
         }
-    
         return ans;
     }
 };
