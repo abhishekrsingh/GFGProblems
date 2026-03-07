e8a4c5360d7df6ab66ff88f7ed7773d843fdd801
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-03-07 20:30:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -1,27 +1,32 @@
 class Solution {
-    public static String smallestWindow(String s, String p) {
+    public static String minWindow(String s, String p) {
         // code here
-        int n=s.length();
-        int[] f=new int[26];
-        int minLen=Integer.MAX_VALUE;
-        int resStart=0;
-        for(char ch: p.toCharArray()) f[ch-'a']++;
-        int count =p.length(),l=0;
-        for(int r=0;r<n;r++){
-            char ch=s.charAt(r);
-            if(f[ch-'a']>0) count--;
-            f[ch-'a']--;
-            while(count==0){
-                if(r-l+1 <minLen){
-                    minLen=r-l+1;
-                    resStart=l;
+        if(p.length()>s.length()) return "";
+        int fre[]=new int[256];
+        for(char c:p.toCharArray()){
+            fre[c]++;
+        }
+        int l=0,c=p.length();
+        int min=Integer.MAX_VALUE;
+        int st=0;
+        for(int i=0;i<s.length();i++){
+            if(fre[s.charAt(i)]>0){
+                c--;
+            }
+            fre[s.charAt(i)]--;
+            while(c==0){
+                if(i-l+1<min){
+                    min=i-l+1;
+                    st=l;
                 }
-                char ch2=s.charAt(l);
-                f[ch2-'a']++;
-                if(f[ch2-'a']>0) count++;
+                fre[s.charAt(l)]++;
+                if(fre[s.charAt(l)]>0){
+                    c++;
+                }
                 l++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(resStart,resStart+minLen);
+        if(min==Integer.MAX_VALUE) return "";
+        return s.substring(st,st+min);
     }
 }
