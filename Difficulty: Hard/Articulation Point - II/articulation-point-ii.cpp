a04e4723caf836e2a64f1d9092a476c0ce4f115f
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-03-28 20:10:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.cpp
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
   public:
@@ -73,30 +67,3 @@ private:
     }
 };
 
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> edges;
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-        Solution obj;
-        vector<int> ans = obj.articulationPoints(V, edges);
-        sort(ans.begin(), ans.end());
-        for (auto i : ans)
-            cout << i << " ";
-        cout << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
