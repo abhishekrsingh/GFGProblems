3e051452b3614b1981e7bdac0ecd8fa6e89616e1
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-03-31 21:29:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.java
+++ Difficulty: Medium/Minimum cost to connect all houses in a city/minimum-cost-to-connect-all-houses-in-a-city.java
@@ -1,100 +1,40 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int[][] edge = new int[n][2];
-            for (int i = 0; i < n; i++) {
-                edge[i][0] = sc.nextInt();
-                edge[i][1] = sc.nextInt();
-            }
-            Solution obj = new Solution();
-            int res = obj.minCost(edge);
-
-            System.out.println(res);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 
 
 class Solution {
 
-    static class DSU{
-        int[] size;
-        int[] parent;
-        
-        DSU(int n){
-            this.parent=new int[n];
-            this.size=new int[n];
-            for(int i=0;i<n;i++){
-                parent[i]=i;
-                size[i]=1;
-            }
-        }
-        
-        int findParent(int node){
-            if(parent[node]==node)
-                return node;
-            
-            return parent[node]=findParent(parent[node]);
-        }
-        
-        void unionBySize(int u,int v){
-            int upu=findParent(u);
-            int upv=findParent(v);
-            
-            if(upu==upv)
-                return;
-            
-            if(size[upu]<size[upv]){
-                parent[upu]=upv;
-                size[upv]+=size[upu];
-            }
-            else{
-                parent[upv]=upu;
-                size[upu]+=size[upv];
-            }
-        }
-    }
-
     public int minCost(int[][] houses) {
-        // code here
-        int n=houses.length;
-        ArrayList<int[]> graph=new ArrayList<>();
+        int n = houses.length;
+        boolean[] vis = new boolean[n];
+        int[] minDist = new int[n];
         
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int u=i,v=j;
-                int wt=Math.abs(houses[i][0]-houses[j][0])+Math.abs(houses[i][1]-houses[j][1]);
-                graph.add(new int[]{u,v,wt});
-            }
-        }
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+        minDist[0] = 0;
         
-        Collections.sort(graph,(a,b)->a[2]-b[2]);
-        int sum = 0;
-        DSU ds=new DSU(n);
-        for(int[] e: graph){
-            int u=e[0];
-            int v=e[1];
-            int wt=e[2];
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int u = -1;
             
-            if(ds.findParent(u) != ds.findParent(v)){
-                sum+=wt;
-                ds.unionBySize(u,v);
+            // pick unvisited node with minimum connection cost
+            for (int j = 0; j < n; j++) {
+                if (!vis[j] && (u == -1 || minDist[j] < minDist[u])) {
+                    u = j;
+                }
+            }
+            
+            vis[u] = true;
+            ans += minDist[u];
+            
+            // update distances of remaining nodes
+            for (int v = 0; v < n; v++) {
+                if (!vis[v]) {
+                    int cost = Math.abs(houses[u][0] - houses[v][0]) 
+                             + Math.abs(houses[u][1] - houses[v][1]);
+                    minDist[v] = Math.min(minDist[v], cost);
+                }
             }
         }
         
-        return sum;
+        return ans;
     }
 }
