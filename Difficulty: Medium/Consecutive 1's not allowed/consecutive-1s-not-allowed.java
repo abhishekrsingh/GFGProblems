41ab749e0a6818cb31c549d51ba102d02a7bc343
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-04-02 11:05:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.java
@@ -0,0 +1,12 @@
+class Solution {
+    int countStrings(int n) {
+        // code here
+        int x=1, y=2;
+        for(int i=2; i<=n; i++){
+            int z=x+y;
+            x=y;
+            y=z;
+        }
+        return y;
+    }
+}
