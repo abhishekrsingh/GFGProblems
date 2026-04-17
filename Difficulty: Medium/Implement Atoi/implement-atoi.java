6ef6abd21ae72c9d56d10eb41170d4911d598610
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-04-17 20:43:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implement Atoi/implement-atoi.java
+++ Difficulty: Medium/Implement Atoi/implement-atoi.java
@@ -1,57 +1,67 @@
-//{ Driver Code Starts
-// Initial template for JAVA
-
-import java.util.Scanner;
-
-class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine();
-        while (t > 0) {
-            String str = sc.nextLine();
-
-            Solution obj = new Solution();
-            int num = obj.myAtoi(str);
-            System.out.println(num);
-            System.out.println("~");
-            t--;
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    public int myAtoi(String s) {
-        int sign = 1, res = 0, idx = 0;
-
-        // Ignore leading whitespaces
-        while (idx < s.length() && s.charAt(idx) == ' ') {
-            idx++;
+    public int myAtoi(String in) {
+        // code here
+        String s= in.replaceAll(" ","");
+        
+        int n=s.length();
+        
+        if(n==0)
+        return 0;
+        
+        if(n==1&&(s.charAt(0)=='-'||s.charAt(0)=='+'))
+        return 0;
+        
+        int maxMin=-2147483648;
+        int maxMax= 2147483647;
+        long ans=0;
+        if(s.charAt(0)=='-')
+        {
+           for(int i=1;i<n;i++)
+           {
+               int c=s.charAt(i)-'0';
+               if((c<0||c>9))
+               return (int)ans*-1;
+               
+               ans=ans*10+(s.charAt(i)-'0');
+               
+               if(ans>maxMax)
+               return maxMin;
+           }
+           ans=ans*-1;
+           
+           return (int) ans;
         }
-
-        // Store the sign of number
-        if (idx < s.length() && (s.charAt(idx) == '-' 
-                                 || s.charAt(idx) == '+')) {
-            if (s.charAt(idx++) == '-') {
-                sign = -1;
+        else{
+            if(s.charAt(0)=='+')
+            {
+             for(int i=1;i<n;i++)
+           {
+               
+                int c=s.charAt(i)-'0';
+               if((c<0||c>9))
+               return (int)ans;
+  ans=ans*10+(s.charAt(i)-'0');               
+               if(ans>=maxMax)
+               return maxMax;
+           }
+           
+           return (int)ans;
+            }
+            else{
+               for(int i=0;i<n;i++)
+           {
+               
+                int c=s.charAt(i)-'0';
+               if((c<0||c>9))
+               return (int)ans;
+  ans=ans*10+(s.charAt(i)-'0');               
+               if(ans>=maxMax)
+               return maxMax;
+           }
+           
+           return (int)ans;   
             }
         }
-
-        // Construct the number digit by digit
-        while (idx < s.length() && s.charAt(idx) >= '0' 
-                                       && s.charAt(idx) <= '9') {
-            
-            // Handling overflow/underflow test case
-            if (res > Integer.MAX_VALUE / 10 || 
-                   (res == Integer.MAX_VALUE / 10 && s.charAt(idx) - '0' > 7)) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-          
-            // Append current digit to the result
-            res = 10 * res + (s.charAt(idx++) - '0');
-        }
-        return res * sign;
+  
     }
 }
