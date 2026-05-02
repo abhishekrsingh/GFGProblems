227a26c68245f6b354e03b52da3e8a8dbcfb9a18
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-05-02 21:45:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Position of the Set Bit/position-of-the-set-bit.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int findPosition(int N) {
+        // code here
+        if(N==0)return-1;
+        if((N&N-1) == 0)return log2(N)+1; 
+        return -1;
+    }
+};
