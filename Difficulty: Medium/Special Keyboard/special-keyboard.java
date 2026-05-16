d445969646ad6f0733e885e30676a45f8f619cf0
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-05-16 16:28:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Special Keyboard/special-keyboard.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int optimalKeys(int n) {
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 1; j <= i - 3; j++) {
+
+                dp[i] = Math.max(dp[i],dp[j] * (i - j - 1));
+            }
+        }
+
+        return dp[n];
+    }
+}
