0e91b5dce90dbe6c59e87249bb7a8287e3d2b70e
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-05-28 21:25:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Wifi Range/wifi-range.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wifiRange(String s, int x) {
+        int size = s.length();
+        
+        for(int i = 0; i < size; i++) {
+            if(s.charAt(i) == '0') {
+                int low = i;
+                
+                while(i+1 < size && s.charAt(i+1) == '0') {
+                    i++;
+                }
+                
+                // If string is all '0's or starts or ends at zero or then range is through one side of '1'
+                // If 0 is somewhere in between then both side of '1's will cover the range
+                if( (low == 0 && i == size-1) || (low == 0 && i - low + 1 > x) || (i == size-1 && i - low + 1 > x) || (i - low + 1 > 2*x) ) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
