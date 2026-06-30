25a2f6b4a9103e278e96d8d5f409565e6028e4a8
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-06-30 07:36:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -1,63 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
 class Solution {
   public:
     bool twoSum(vector<int>& arr, int target) {
-        // Create an unordered_set to store the elements
-        unordered_set<int> s;
+        // code here
+        unordered_set<int> st;
     
-        // Iterate through each element in the vector
-        for (int i = 0; i < arr.size(); i++){
-    
-            // Calculate the complement that added to
-            // arr[i], equals the target
-            int complement = target - arr[i];
-    
-            // Check if the complement exists in the set
-            if (s.find(complement) != s.end())
+        for(auto x: arr) {
+            int remaining = target - x;
+            
+            // Agar (target - x) pehle se set mein hai, toh pair mil gaya!
+            if(st.count(remaining)) {
                 return true;
-    
-            // Add the current element to the set
-            s.insert(arr[i]);
+            }
+            
+            // Agar nahi mila, toh current element ko set mein daal do
+            st.insert(x); // FIX: st[x]++ ki jagah .insert() use kiya
         }
-      
-        // If no pair is found
         return false;
     }
+    
+    // bool twoSum(vector<int>& arr, int target) {
+    //     // code here
+    //     for(int i=0; i<arr.size(); i++)
+    //     {
+    //         for(int j=i+1; j<arr.size(); j++)
+    //         {
+    //             if(arr[i] + arr[j] == target) return true; 
+    //         }
+    //     }
+    //     return false;
+    // }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int x;
-        cin >> x;
-        cin.ignore();
-
-        vector<int> arr;
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        auto ans = ob.twoSum(arr, x);
-        cout << (ans ? "true" : "false") << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
