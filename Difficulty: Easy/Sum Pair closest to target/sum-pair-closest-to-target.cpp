6fe13fdc10333756261461a5332de0e417bc6d38
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-07-02 07:46:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.cpp
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.cpp
@@ -1,63 +1,35 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
 class Solution {
   public:
     vector<int> sumClosest(vector<int>& arr, int target) {
-        sort(arr.begin(),arr.end());
-        int s=0,e=arr.size()-1;
-        int diff=INT_MAX;
-        vector<int>ans;
-        while(s<e){
-            int pairsum=arr[s]+arr[e];
-            if(abs(target-pairsum)<diff){
-            diff=min(abs(target-pairsum),diff);
-            ans={arr[s],arr[e]};}
-            if(pairsum<target) s++;
-            else if(pairsum>target) e--;
-            else return ans;
+        // code here
+        int n = arr.size();
+        if(n < 2) return {};
+        sort(arr.begin(), arr.end());
+        vector<int> result;
+        int minDiffFromTarget = 1e9;
+        int maxPairDiff = -1;
+        int left=0, right=n-1;
+        while(left < right)
+        {
+            int currentSum = arr[left] + arr[right];
+            int currentMinDifference = abs(target - currentSum);
+                
+            if(currentMinDifference < minDiffFromTarget)
+            {
+                minDiffFromTarget = currentMinDifference;
+                maxPairDiff = arr[right] - arr[left];
+                result = {arr[left], arr[right]};
+            }
+            if(currentMinDifference == minDiffFromTarget)
+            {
+                if(arr[right] - arr[left] > maxPairDiff){
+                    maxPairDiff = arr[right] - arr[left];
+                    result = {arr[left], arr[right]};
+                }
+            }
+                
+            (currentSum < target)? left++ : right--;
         }
-        return ans;
+        return result;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int target = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.sumClosest(arr, target);
-        if (ans.empty()) {
-            cout << "[]";
-        } else {
-            for (auto it : ans) {
-                cout << it << " ";
-            }
-        }
-        cout << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-// } Driver Code Ends
