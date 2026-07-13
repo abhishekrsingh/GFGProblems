1a1a2de88db91d9975db582dbf643b7d6f253d73
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-07-13 08:29:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
@@ -1,58 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-
-    // Function to return length of longest subsequence of consecutive integers.
     int longestConsecutive(vector<int>& arr) {
-        int freq[1000001]={0};
-        int count=0;
-        int ans=INT_MIN;
-        int maxi=-1;
-        for(int i=0;i<arr.size();i++){
-            freq[arr[i]]++;
-            maxi=max(maxi,arr[i]);
-        }
-        for(int i=0;i<=maxi;i++){
-            if(freq[i]>0) count++;
-            else{
-                ans=max(ans,count);
-                count=0;
+        // code here
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int maxLen = 1;
+        int currentLen = 1;
+        for(int i=0; i<n-1; i++)
+        {
+            int diff = arr[i+1] - arr[i];
+            if(diff == 1){
+                currentLen++;
+                maxLen = max(maxLen, currentLen);
+            }else if(diff == 0){
+                continue;
+            }else{
+                currentLen = 1;
             }
-            ans=max(ans,count);
         }
-        return ans;
+        return maxLen;
     }
+    
+    //1 9 3 10 4 20 2 
+    //1 2 3 4 9 10 20
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.longestConsecutive(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
