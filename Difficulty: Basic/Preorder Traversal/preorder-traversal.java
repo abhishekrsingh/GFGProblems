a009500a238a0c4f2fe8c863ef97748642d6ccec
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-07-24 16:32:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.java
@@ -0,0 +1,31 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public void helper(Node root, ArrayList<Integer> preOrder){
+        if(root == null){
+            return;
+        }
+        
+        preOrder.add(root.data);
+        helper(root.left, preOrder);
+        helper(root.right, preOrder);
+    }
+    
+    public ArrayList<Integer> preOrder(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        return ans;
+    }
+}
