58ced6dfd1494fc78d6d6004b92922e3fae099c8
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-09-21 16:27:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -2,41 +2,28 @@ class Solution {
   public:
     vector<int> subarraySum(vector<int> &arr, int target) {
         // code here
-        int n=arr.size();
-        int i=0, j=0, current_sum=0;
-        while(j<n)
-        {
-            current_sum += arr[j];
-            while(current_sum > target && i<j)
-            {
-                current_sum -= arr[i];
-                i++;
-            }
-            if(current_sum == target){
-                return {i+1, j+1};
-            }
-            j++;
+        int start=0,currentSum=0;
+        for(int end=0; end<arr.size(); end++){
+            currentSum += arr[end];
+            while(currentSum > target && start < end) currentSum -= arr[start++];
+            if(currentSum == target) return {start+1, end+1};
         }
-        
         return {-1};
     }
     
-    //O(n^2)
+    
     // vector<int> subarraySum(vector<int> &arr, int target) {
     //     // code here
-    //     int n=arr.size();
-    //     vector<int> result;
+    //     int n = arr.size();
     //     for(int i=0; i<n; i++){
     //         int currentSum = 0;
-    //         for(int j=i; j<n; j++)
-    //         {
+    //         for(int j=i; j<n; j++){
     //             currentSum += arr[j];
     //             if(currentSum == target){
-    //                 result.push_back(i+1);
-    //                 result.push_back(j+1);
-    //                 return result;
+    //                 return {i+1, j+1};
     //             }
     //         }
+            
     //     }
     //     return {-1};
     // }
